594584a368e66d23d0eb106e7c2bc793fc45579f
Artur Rashkevich <arashkevich25@gmail.com>
2025-08-21 08:58:34 +0200

    feat(common): Use common crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,13 @@
-use std::collections::HashMap;
-
 use log::{info, warn};
+use notifica_common::configuration::WebhookDto;
 use reqwest::StatusCode;
-use serde::{Deserialize, Serialize};
 use serde_json::json;
 use uuid::Uuid;
 
 pub struct Notifica;
 
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct NotificaBodyEmail {
-    pub target_email: Option<String>,
-    pub target_name: Option<String>,
-    pub sender_name: Option<String>,
-    pub subject: Option<String>,
-    pub params: HashMap<String, String>,
-}
-
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct NotificaBody {
-    pub push: Option<HashMap<String, String>>,
-    pub email: Option<NotificaBodyEmail>,
-    pub request: Option<HashMap<String, String>>,
-}
-
 impl Notifica {
-    pub async fn send_message(body: NotificaBody, event: &str, tenant_id: Uuid) {
+    pub async fn send_message(body: WebhookDto, event: &str, tenant_id: Uuid) {
         info!("[NOTIFICA-SDK] Tenant '{tenant_id}' event '{event}'");
         let client = reqwest::Client::new();
         let response: reqwest::Response = client
